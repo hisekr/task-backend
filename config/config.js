597b5bbffeaf30c32f3499e9d6b3ff7e00e40fd5
Abhishek roshan <abhishekroshan11@gmail.com>
2024-04-06 11:18:49 +0530

    Updated mysql

--- config/config.js
+++ config/config.js
@@ -1,28 +1,56 @@
-// const Sequelize = require("sequelize");
+// // const Sequelize = require("sequelize");
 
-// const database = new Sequelize({
-//   database: "varthana",
-//   username: "root",
-//   password: "123456",
-//   dialect: "mysql",
-// });
-//
-// module.exports = database;
+// // const database = new Sequelize({
+// //   database: "varthana",
+// //   username: "root",
+// //   password: "123456",
+// //   dialect: "mysql",
+// // });
+// //
+// // module.exports = database;
+
+// // const Sequelize = require("sequelize");
+// // const dotenv = require("dotenv");
+
+// // dotenv.config();
+
+// // const database = new Sequelize({
+// //   host: "localhost",
+// //   database: process.env.DATABASE,
+// //   // username: process.env.USERNAME,
+// //   username: "root",
+// //   password: process.env.PASSWORD,
+// //   dialect: "mysql",
+// //   port: 3306,
+// // });
+
+// // module.exports = database;
 
 // const Sequelize = require("sequelize");
 // const dotenv = require("dotenv");
 
 // dotenv.config();
 
-// const database = new Sequelize({
-//   host: "localhost",
-//   database: process.env.DATABASE,
-//   // username: process.env.USERNAME,
-//   username: "root",
-//   password: process.env.PASSWORD,
-//   dialect: "mysql",
-//   port: 3306,
-// });
+// const database = new Sequelize(
+//   process.env.DATABASE,
+//   process.env.USERNAME,
+//   process.env.PASSWORD,
+//   {
+//     host: monorail.proxy.rlwy.net,
+//     dialect: "mysql",
+//     port: 49940,
+//     pool: {
+//       max: 10,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000,
+//     },
+//     // dialectOptions: {
+//     //   socketPath: "/opt/lampp/var/mysql/mysql.sock",
+//     // },
+//     logging: true,
+//   }
+// );
 
 // module.exports = database;
 
@@ -32,22 +60,19 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const database = new Sequelize(
-  process.env.DATABASE,
-  process.env.USERNAME,
-  process.env.PASSWORD,
+  process.env.MYSQLDATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQLPASSWORD,
   {
-    host: "localhost",
+    host: process.env.MYSQLHOST,
     dialect: "mysql",
-    port: 3306,
+    port: process.env.MYSQLPORT,
     pool: {
       max: 10,
       min: 0,
       acquire: 30000,
       idle: 10000,
     },
-    // dialectOptions: {
-    //   socketPath: "/opt/lampp/var/mysql/mysql.sock",
-    // },
     logging: true,
   }
 );
